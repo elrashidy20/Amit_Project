a3670d7dc51ef5c0330f258aa42632cb12503bd5
Elrashidy <elrashidy20@gmail.com>
2021-03-08 23:16:17 +0200

    Upload Files

--- app/src/main/java/com/example/finalproject/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/finalproject/ui/fragment/ProfileFragment.java
@@ -1,14 +1,19 @@
 package com.example.finalproject.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.finalproject.R;
+import com.example.finalproject.ui.activity.Login;
 
 public class ProfileFragment extends Fragment {
 
@@ -18,7 +23,7 @@ public class ProfileFragment extends Fragment {
     }
 
 
-
+Button LogOut;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +35,20 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LogOut=view.findViewById(R.id.button_Log_out);
+        LogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity().getApplication(), Login.class);
+                startActivity(intent);
+                getActivity().finishAffinity();
+            }
+        });
+
     }
 }
